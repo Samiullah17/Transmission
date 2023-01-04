cd0c8da287d1c2e89143ddf0dbec67fe5184af49
Ajmal Nasiri <106055460+AjmalNasiri@users.noreply.github.com>
2023-01-04 10:10:32 +0430

    samai and Ajmal merge done by Ajmal

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -4,12 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 
 class UserController extends Controller
 {
     public function index()
     {
         $users=User::get();
-        return view('users.index',compact('users'));
+        $permissions=Permission::get();
+        return view('users.index',compact('users','permissions'));
+    }
+    public function destroy($id)
+    {
+        
+        $user=User::find($id);
+        if($user->status==0)
+        {
+        $user->status=1;
+        $user->update();
+        }
+        else{
+            $user->status=0;
+            $user->update();
+        }
+        return response()->json(['success'=>route('list.user')]);
     }
 }
